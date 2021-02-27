838b6d83e259ef23dfe58c26c4c066f300b6265d
szymwisn <szymwisn@gmail.com>
2021-02-27 16:54:10 +0100

    Scrollable menu

--- static/js/base.js
+++ static/js/base.js
@@ -3,7 +3,17 @@ let menu = document.querySelector(".menu");
 
 const handleHamburger = () => {
   hamburger.classList.toggle("cross");
-  menu.style.display = menu.style.display === "none" ? 'block': 'none';
-}
+  menu.style.display = menu.style.display === "none" ? "block" : "none";
+};
 
+(() => {
+  const nav = document.getElementsByTagName("nav")[0];
 
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 0) {
+      nav.classList.add("scrolled");
+    } else {
+      nav.classList.remove("scrolled");
+    }
+  });
+})();
