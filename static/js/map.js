18fa4e5f873b83e987908fc545938ef859a3dd19
szymwisn <szymwisn@gmail.com>
2021-02-28 12:06:15 +0100

    Finished contacts and some overall style adjustments

--- static/js/map.js
+++ static/js/map.js
@@ -1,6 +1,45 @@
-const mymap = L.map("mapid").setView([54.1756, 15.58122], 30);
+(() => {
+  function prepareMap() {
+    const map = L.map("mapid", {
+      minZoom: 10,
+      maxZoom: 18,
+    }).setView([54.1756, 15.58122], 18);
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(mymap);
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(
+      map
+    );
 
-const marker = L.marker([54.1756, 15.58122]).addTo(mymap);
-marker.bindPopup("Kołobrzeg, 78-100 ul. Słowińców 9/41").openPopup();
+    const marker = L.marker([54.1756, 15.58122], {
+      icon: L.divIcon({
+        className: "marker",
+        iconSize: [24, 24],
+        popupAnchor: [160, 39],
+      }),
+    });
+
+    marker.bindPopup("Kołobrzeg, ul. Słowińców 9/41").openPopup();
+
+    marker.on("mouseover", (e) => {
+      marker.openPopup();
+    });
+
+    marker.on("mouseout", (e) => {
+      marker.closePopup();
+    });
+
+    marker.addTo(map);
+  }
+
+  function prepareMapButton() {
+    const mapButton = document.getElementsByClassName("map-button")[0];
+    const mapEl = document.getElementById("mapid");
+
+    mapButton.addEventListener("click", () => {
+      mapEl.scrollIntoView();
+      window.scrollBy(0, -140);
+    });
+  }
+
+  prepareMap();
+  prepareMapButton();
+})();
